8a08b2ff6c9c7c05f5204efc82ce5cf32ca323e5
eman <88867369+EmanAboghaba@users.noreply.github.com>
2025-05-23 19:36:10 +0300

    Delete src/app directory

--- src/app/_lib/dbConnection.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-export function dbConnection() {
-  mongoose
-    .connect("mongodb://localhost:27017/EcommerceSystem")
-    .then(() => console.log("Database connected successfully"))
-    .catch((err) => console.error("Database connection error:", err));
-}
